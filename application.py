79a80c5f49e3939159381a876bc8de9282461a45
GirishSawantoo7 <154802626+GirishSawantoo7@users.noreply.github.com>
2024-09-04 01:23:47 +0530

    application change

--- application.py
+++ application.py
@@ -1,30 +1,25 @@
-from flask import Flask, request, app,render_template
-from flask import Response
+from flask import Flask, request, render_template
 import pickle
+import os
 import numpy as np
-import pandas as pd
-
 
 application = Flask(__name__)
-app=application
+app = application
 
-scaler=pickle.load(open("Model\standardScalar.pkl", "rb"))
-model = pickle.load(open("Model\modelForPrediction.pkl", "rb"))
-
-## Route for homepage
+# Construct the file paths dynamically
+model_dir = os.path.join(os.path.dirname(__file__), 'Model')
+scaler = pickle.load(open(os.path.join(model_dir, 'standardScalar.pkl'), 'rb'))
+model = pickle.load(open(os.path.join(model_dir, 'modelForPrediction.pkl'), 'rb'))
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-## Route for Single data point prediction
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    result=""
-
-    if request.method=='POST':
-
-        Pregnancies=int(request.form.get("Pregnancies"))
+    result = ""
+    if request.method == 'POST':
+        Pregnancies = int(request.form.get("Pregnancies"))
         Glucose = float(request.form.get('Glucose'))
         BloodPressure = float(request.form.get('BloodPressure'))
         SkinThickness = float(request.form.get('SkinThickness'))
@@ -33,19 +28,17 @@ def predict_datapoint():
         DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
         Age = float(request.form.get('Age'))
 
-        new_data=scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
-        predict=model.predict(new_data)
+        new_data = scaler.transform([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        predict = model.predict(new_data)
        
-        if predict[0] ==1 :
+        if predict[0] == 1:
             result = 'Diabetic'
         else:
-            result ='Non-Diabetic'
+            result = 'Non-Diabetic'
             
-        return render_template('single_prediction.html',result=result)
-
+        return render_template('single_prediction.html', result=result)
     else:
         return render_template('home.html')
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
